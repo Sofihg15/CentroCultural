dabfd1bbd9aea2a5e2ac7b10e155e22afb231d70
Sofihg15 <hernandez.gonzalez.alma.sofia03@gmail.com>
2025-06-30 23:03:29 -0600

    Update Actividad.java

--- Actividad.java
+++ Actividad.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.centrocultural;
-
-/**
- *
- * @author almasofiahernandez
- */
 import java.util.ArrayList;
 
 public class Actividad {
@@ -61,3 +51,4 @@ public class Actividad {
         alumnosInscritos.add(numeroExpediente);
     }
 }
+
