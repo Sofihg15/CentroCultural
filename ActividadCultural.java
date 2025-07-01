70a5e3f6e09c899219bf63c133311d763e6db448
Sofihg15 <hernandez.gonzalez.alma.sofia03@gmail.com>
2025-06-30 23:04:07 -0600

    Update ActividadCultural.java

--- ActividadCultural.java
+++ ActividadCultural.java
@@ -1,20 +1,2 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.centrocultural;
 
-/**
- *
- * @author almasofiahernandez
- */
-public class ActividadCultural extends Actividad {
-
-    public ActividadCultural(String nombre, String instructorResponsable, 
-                             String areaFisicaAsignada, String areaAlmacenamientoAsignada) {
-        // Llama al constructor de la clase padre
-        super(nombre, "Cultural", instructorResponsable, areaFisicaAsignada, areaAlmacenamientoAsignada);
-    }
-
-}
 
